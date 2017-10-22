1dcf667184bc972a56997fabada01480e0a7af9e
Vespazian Crai <vespaziancrai@gmail.com>
2017-10-22 13:05:06 -0400

    00->05

---
+++ day08/ex05/ft_stock_par.h
@@ -0,0 +1,18 @@
+#ifndef FT_STOCK_PAR_H
+# define FT_STOCK_PAR_H
+# include <stdlib.h>
+
+char		**ft_split_whitespaces(char *str);
+typedef struct		s_stock_par
+{
+	int		size_param;
+	char	*str;
+	char	*copy;
+	char	**tab;
+}	t_stock_par;
+
+void		ft_show_tab(struct s_stock_par *par);
+
+struct s_stock_par	*ft_param_to_tab(int argc, char **argv);
+
+#endif
